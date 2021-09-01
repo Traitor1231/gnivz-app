cb25889bc5d722bf1dc11ec9225805d1e33291b2
Nikita Pereskokov <nikita234mail.ru@mail.ru>
2021-09-01 22:42:32 +0400

    Refactoring

--- src/components/EmailPopup.tsx
+++ src/components/EmailPopup.tsx
@@ -1,11 +1,9 @@
-import React, { FC, useState } from "react";
+import { FC, useState } from "react";
 import Popover from "@material-ui/core/Popover";
-import Typography from "@material-ui/core/Typography";
 import { EmailPopupProps } from "../interfaces/EmailPopupProps";
 import { useEmailPopupStyles } from "../styles/EmailPopupStyles";
 import { getPostCountByUser } from "../utils/getPostCountByUser";
 import { Box } from "@material-ui/core";
-
 
 export const EmailPopup: FC<EmailPopupProps> = ({ id, email }) => {
   const classes = useEmailPopupStyles();
@@ -15,22 +13,18 @@ export const EmailPopup: FC<EmailPopupProps> = ({ id, email }) => {
   const [postCount, setPostCount] = useState("");
 
   const onHoverHandler = (id: number) =>
-      getPostCountByUser(id)
-      .then((result) =>
-        setPostCount(`Количество постов: ${result.data.data.length}`)
-      );
+    getPostCountByUser(id).then((result) =>
+      setPostCount(`Количество постов: ${result.data.data.length}`)
+    );
 
   return (
-    <React.Fragment>
-      <Typography
-        onMouseEnter={(e) => setAnchor(e.currentTarget)}
-        onMouseLeave={() => setAnchor(null)}
-        aria-owns={anchor ? "mouse-over-popover" : undefined}
-        aria-haspopup="true"
-      >
-        <Box component="span" onMouseOver={() => onHoverHandler(id)}>{email}</Box>
-      </Typography>
-      {postCount && (
+    <Box
+      onMouseEnter={(e) => setAnchor(e.currentTarget)}
+      onMouseLeave={() => setAnchor(null)}
+      onMouseOver={() => onHoverHandler(id)}
+      component="span"
+    >
+      {[email, postCount && (
         <Popover
           className={classes.popover}
           classes={{
@@ -38,18 +32,10 @@ export const EmailPopup: FC<EmailPopupProps> = ({ id, email }) => {
           }}
           open={!!anchor}
           anchorEl={anchor}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
         >
-          <Typography>{postCount}</Typography>
+          {postCount}
         </Popover>
-      )}
-    </React.Fragment>
+      )]}
+    </Box>
   );
 };
